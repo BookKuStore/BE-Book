d81f58e8d02e62e37517b7e8add04fb8ec5bd154
Ethanic <emirmfathan@gmail.com>
2024-04-24 00:23:29 +0700

    adding jbdc

--- src/main/java/com/example/book_bookku/BookBookkuApplication.java
+++ src/main/java/com/example/book_bookku/BookBookkuApplication.java
@@ -2,8 +2,9 @@ package com.example.book_bookku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BookBookkuApplication {
 
 	public static void main(String[] args) {
