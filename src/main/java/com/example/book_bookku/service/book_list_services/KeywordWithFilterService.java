107262a05ba5f00f8964cdc87a79a672dc22e0bb
FBimo <f.bimo31@gmail.com>
2024-05-01 18:27:55 +0700

    [REFACTOR]Change searchByName into searchByTitle method call

--- src/main/java/com/example/book_bookku/service/book_list_services/KeywordWithFilterService.java
+++ src/main/java/com/example/book_bookku/service/book_list_services/KeywordWithFilterService.java
@@ -12,7 +12,7 @@ public class KeywordWithFilterService extends BookListService {
     public List<Book> handleRequest() {
         if (filterBy != null) {
             if (Objects.equals(filterBy, "judul")) {
-                return bookRepository.searchByName(keyword);
+                return bookRepository.searchByTitle(keyword);
             } else if (Objects.equals(filterBy, "penulis") || Objects.equals(filterBy, "pengarang")) {
 //                System.out.println("halo");
                 return bookRepository.searchByAuthor(keyword);
