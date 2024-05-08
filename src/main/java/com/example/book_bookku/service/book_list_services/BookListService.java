943552b6bbde888c34ed808f5b4def39e99a1676
FBimo <f.bimo31@gmail.com>
2024-05-08 20:52:30 +0700

    [REFACTOR] Set new function to accomodate sort feature

--- src/main/java/com/example/book_bookku/service/book_list_services/BookListService.java
+++ src/main/java/com/example/book_bookku/service/book_list_services/BookListService.java
@@ -5,6 +5,7 @@ import com.example.book_bookku.repository.BookRepository;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,7 +22,21 @@ public abstract class BookListService {
     protected String keyword;
     @Setter@Getter
     protected String filterBy;
+    @Setter@Getter
+    protected String sortBy;
+    @Setter@Getter
+    protected String sortDir;
+    @Setter@Getter
+    protected Sort sort;
 
     public abstract List<Book> handleRequest();
+
+    public void setSorting() {
+        if (this.sortDir.equals("desc")) {
+            sort = Sort.by(this.sortBy).descending();
+        } else {
+            sort = Sort.by(this.sortBy).ascending();
+        }
+    }
 
 }
