a69e31cee5a401e942fce990bd1a926cdf7a6902
Ethanic <emirmfathan@gmail.com>
2024-05-26 01:26:51 +0700

    add cross origin

--- src/main/java/com/example/book_bookku/controller/BookController.java
+++ src/main/java/com/example/book_bookku/controller/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "book")
 public class BookController {
