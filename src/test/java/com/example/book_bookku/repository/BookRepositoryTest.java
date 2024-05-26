d149304006976f085efbdb19ed43f677b1319ff2
Ethanic <emirmfathan@gmail.com>
2024-05-26 14:44:55 +0700

    [GREEN] Implement correct test for BookRepository

--- src/test/java/com/example/book_bookku/repository/BookRepositoryTest.java
+++ src/test/java/com/example/book_bookku/repository/BookRepositoryTest.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 //@SpringBootTest
 //@SpringJUnitConfig
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepositoryTest {
 
     @Autowired
